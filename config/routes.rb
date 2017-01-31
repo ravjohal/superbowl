478c27f71a3427eb5433a057b741120e0fd5e0d1
ravjohal <ravneetjohal@gmail.com>
2017-01-30 23:25:01 -0800

    code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users do
+      get "sign_out", :to => "devise/sessions#destroy"
+  end
   resources :users
   resources :prop_bet_sheets
   resources :super_bowls
