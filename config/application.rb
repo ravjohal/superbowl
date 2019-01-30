618d72cf5ecbc7be7817e4ef7a0d38f0aeb84809
ravjohal <ravneetjohal@gmail.com>
2019-01-29 21:40:57 -0800

    mailing works in dev now

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,13 @@ module Superbowl
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'config.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,15 +42,6 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
-  }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
@@ -58,6 +49,15 @@ Rails.application.configure do
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com',
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
