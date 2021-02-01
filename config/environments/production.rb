5564d4c7f14969b213c5b3d501eec93ffd293daf
ravjohal <ravneetjohal@gmail.com>
2021-01-31 17:52:39 -0800

    updated from settings for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { :from => "reset_your_propbets_password@propbets.herokuapp.com", :reply_to => "noreply@propbets.herokuapp.com"}
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
